c899d61f52c044c998f41df9704421a7fdd9ee16
phani <rajuraju218@gmail.com>
2019-03-20 15:55:07 +0530

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ if [ $ENVIRONMENT = "QA" ];then
         sshpass -p "tomcat" ssh tomcat@172.17.0.2 "JAVA_HOME=/home/tomcat/softs/jdk1.8.0_201" "/home/tomcat/softs/apache-tomcat-8.5.38/bin/startup.sh"
 
 elif  [ $ENVIRONMENT = "SIT" ];then
-        sshpass -p "tomcat" scp target/gamutkart.war tomcat@172.17.0.2:/home/tomcat/softs/apache-tomcat-8.5.38/webapps
-        sshpass -p "tomcat" ssh tomcat@172.17.0.2 "JAVA_HOME=/home/tomcat/softs/jdk1.8.0_201" "/home/tomcat/softs/apache-tomcat-8.5.38/bin/startup.sh"
+        sshpass -p "node1" scp target/gamutkart.war node1@172.17.0.3:/home/node1/softs/apache-tomcat-8.5.38/webapps
+        sshpass -p "node1" ssh node1@172.17.0.3 "JAVA_HOME=/home/node1/softs/jdk1.8.0_201" "/home/node1/softs/apache-tomcat-8.5.38/bin/startup.sh"
 
 echo "deployment has been done!"
 fi
