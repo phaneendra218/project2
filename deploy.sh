2385b62afe677b1df059a516c904465a59cc0882
phani <rajuraju218@gmail.com>
2019-03-20 16:55:52 +0530

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 ##
 ENVIRONMENT=$1
 if [ $ENVIRONMENT = "QA" ];then
-        sshpass -p "tomcat" scp target/gamutkart.war tomcat@172.17.0.2:/home/tomcat/softs/apache-tomcat-8.5.38/webapps
+        sshpass -p "tomcat" scp /home/node2/projects/workspace/sampleFS/target/gamutkart.war tomcat@172.17.0.2:/home/tomcat/softs/apache-tomcat-8.5.38/webapps
 
 elif  [ $ENVIRONMENT = "SIT" ];then
-        sshpass -p "node1" scp target/gamutkart.war node1@172.17.0.3:/home/node1/softs/apache-tomcat-8.5.38/webapps
+        sshpass -p "node1" scp /home/node2/projects/workspace/sampleFS/target/gamutkart.war node1@172.17.0.3:/home/node1/softs/apache-tomcat-8.5.38/webapps
 
 echo "deployment has been done!"
 fi
